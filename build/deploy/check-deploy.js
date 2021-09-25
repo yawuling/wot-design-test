215464298384765f42909d4f14f13cff905c6b8a
zhongzhijie <zhongzhijie@jd.com>
2021-09-25 16:37:22 +0800

    test: check

--- build/deploy/check-deploy.js
+++ build/deploy/check-deploy.js
@@ -5,7 +5,9 @@ const stopDeploy = (reason) => {
 /** ******在此区间进行部署条件校验，不通过时调用stopDeploy停止部署************************/
 
 // 当前的git tag 版本
-const version = process.env.RELEASE_NAME
+const ref = process.env.GITHUB_REF
+const splits = ref.split('/')
+const version = splits[splits.length - 1]
 if (!version) {
   stopDeploy('Can\'t find version to deploy, and stop to deploy !')
 }
@@ -17,7 +19,3 @@ if (version !== `v${require('../../package.json').version}`) {
 if (!process.env.RELEASE_NOTE) {
   stopDeploy('Can\'t find the version\'s release notes of changelog.md')
 }
-
-/** ******在此区间进行部署条件校验，不通过时调用stopDeploy停止部署************************/
-
-process.stdout.write(true + '\n')
